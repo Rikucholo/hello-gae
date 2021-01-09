fdfb8e3f86f490e5968ee106546be9edc047b58a
RikuCholo <starcloud.nal.aircraft@gmail.com>
2021-01-09 19:48:16 +0900

    feat(server):cloud loging にログが出るようにした

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"fmt"
 	"html/template"
+	"context"
+	"log"
+	"cloud.google.com/go/logging"
 	"github.com/labstack/echo"
 	"github.com/ymotongpoo/datemaki"
 )
@@ -18,6 +21,21 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 }
 
 func main() {
+	ctx := context.Background()
+	projectID := "sample-301100"
+	client, err := logging.NewClient(ctx, projectID)
+	if err != nil {
+			log.Fatalf("Failed to create client: %v", err)
+	}
+	logName := "my-log"
+	logger := client.Logger(logName)
+	text := "Hello, world!"
+	logger.Log(logging.Entry{Payload: text})
+	if err := client.Close(); err != nil {
+			log.Fatalf("Failed to close client: %v", err)
+	}
+	fmt.Printf("Logged: %v\n", text)
+	
 	e := echo.New()
 	e.Renderer = &Template{
         templates: template.Must(template.ParseGlob("front/*.html")),
@@ -26,7 +44,8 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(http.StatusOK,"index",nil)
 	})
-	e.GET("/date", parseDate)
+	l := e.GET("/date", parseDate)
+	e.Logger.Printf("結果： %s", l)
 	port := os.Getenv("PORT")
     if port == "" {
         port = "8080"
@@ -37,7 +56,9 @@ func main() {
 func parseDate(c echo.Context) error {
 	date, err := datemaki.Parse(c.QueryParam("value"))
 	if err != nil {
+		fmt.Printf("REQUESTされた結果は: %v\n", "日付ではありません")
 		return c.JSON(http.StatusOK, "日付ではありません")
 	}
+	fmt.Printf("REQUESTされた結果は: %v\n",date)
 	return c.JSON(http.StatusOK, date)
 }
